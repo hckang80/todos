3005539e0334823a20987dc1124adab73d29fb25
hckang <hckang80@gmail.com>
2018-06-08 11:01:47 +0900

    기능 추가 1. 한 일, 할 일 토글하여 보는 기능 2. 완료한 일 삭제 기능 3. 한 일과 할 일의 목록 갯수 표시 기능

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row">
+    <div class="col-xs-6">
+      <label class="i-checks">
+        <input type="checkbox" v-model="completed" @change="toggleTodosCompleted(completed)" />
+        <i />
+        <span>Mark all as complete</span>
+      </label>
+    </div>
+    <div class="col-xs-6 text-right">
+      <button class="btn btn-default btn-xs" @click="clearTodoCompleted">
+        Clear completed (<span>{{ completedTodoLength }}</span>)
+      </button>
+      <strong>{{ leftTodoLength }}</strong> item left
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+      completed: false
+    }
+  },
+  methods: {
+    toggleTodosCompleted(completed) {
+      this.$store.commit('toggleTodosCompleted', completed);
+    },
+    ...mapMutations([
+      'clearTodoCompleted'
+    ])
+  },
+  computed: {
+    ...mapGetters([
+      'completedTodoLength'
+    ]),
+    ...mapGetters([
+      'leftTodoLength'
+    ])
+  }
+}
+</script>
+
